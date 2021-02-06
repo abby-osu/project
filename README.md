df4d24d9099614819a871f08f20968537c1a3308
abby-osu <76971382+abby-osu@users.noreply.github.com>
2021-02-06 12:05:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Hello! 
+
+I got all my pictures from Unsplash, a website for freely-usable images: 
+https://unsplash.com/. 
+
+All the pictures I used can be downloaded and used for free. 
+Here is their policy: https://unsplash.com/license
+
+
+I got the birthday song from the Free Music Archive: 
+It's the "It's Your Birthday" by The Blank Tapes. 
+You can find it here: https://www.freemusicarchive.org/music/Happy_Birthday_Song_Contest/The_New_Birthday_Song_Contest
---
+++ script.js
@@ -0,0 +1,23 @@
+function openTab(event, tabName){
+
+    var i;
+    var tabcontent;
+    var tablinks;
+
+    // hide all the tabcontents, otherwise it will show up as one whole HTML doc
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for( var i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display ="none";
+    }
+
+    //
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(var i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
